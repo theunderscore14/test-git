670ea429461892ab61aa24f88ae3d698fbccee4b
Shrenik Abrol <shrenikabrol8@gmail.com>
2026-04-11 10:42:02 +0530

    Java Testing

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 10; i > 0; i--) {
-            System.out.println("Hello, World! " + i);
-        }
-        System.out.println("Liftoff!");
+        String name = "John";
+        System.out.println("Hello " + name);
     }
 }
